51a97c6d3e7e4ba9c4417b9a95e61f481a58c1ae
DIP <72380665+aunindasaha@users.noreply.github.com>
2023-04-02 16:19:34 +0600

    One push Signal and Easyloading

---
+++ lib/Apicalls/fetchcatagories.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:doctorappointment/Modelclasses/viewSpecialistModel.dart';
+import 'package:http/http.dart' as http;
+
+
+Future<List<catagoryModel>> fetchCategories() async {
+  return Future.delayed(Duration(seconds: 2), () async {
+    var url = Uri.parse("https://dms.symbexit.com/api/viewSpecialist");
+    var data = await http.get(url);
+    var jsonData = json.decode(data.body);
+    final list = jsonData as List<dynamic>;
+
+    return list.map((e) => catagoryModel.fromJson(e)).toList();
+  });
+
+
+
+}
